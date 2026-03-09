45ba5f30dd01e61ad822f255e8e231000426950e
Tomas Bruckner <tomasbrucknermail@gmail.com>
2026-03-09 20:42:22 +0100

    test: add true-positive tests for additional analyzer scenarios
    Add four new tests covering property initializers, cross-method field
    assignments, nullable value types, and object initializer assignments.

--- tests/NotNullAnalyzer.Tests/NullableAssignmentAnalyzerTests.cs
+++ tests/NotNullAnalyzer.Tests/NullableAssignmentAnalyzerTests.cs
@@ -44,4 +44,108 @@ public class MyClass
         };
         await test.RunAsync();
     }
+
+    [Fact]
+    public async Task NullableProperty_WithNonNullableInitializer_Reports()
+    {
+        var test = new CSharpAnalyzerTest<NullableAssignmentAnalyzer, DefaultVerifier>
+        {
+            TestCode = @"
+#nullable enable
+public class MyClass
+{
+    public string? {|#0:Name|} { get; set; } = ""default"";
+}
+",
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(DiagnosticDescriptors.NOTNULL001)
+                    .WithLocation(0)
+                    .WithArguments("Name"),
+            },
+        };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task NullableField_AssignedNonNullableAcrossMethods_Reports()
+    {
+        var test = new CSharpAnalyzerTest<NullableAssignmentAnalyzer, DefaultVerifier>
+        {
+            TestCode = @"
+#nullable enable
+public class MyClass
+{
+    private string? {|#0:_name|};
+
+    public void SetFromA(string a) { _name = a; }
+    public void SetFromB(string b) { _name = b; }
+}
+",
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(DiagnosticDescriptors.NOTNULL001)
+                    .WithLocation(0)
+                    .WithArguments("_name"),
+            },
+        };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task NullableValueType_AssignedLiteralOnly_Reports()
+    {
+        var test = new CSharpAnalyzerTest<NullableAssignmentAnalyzer, DefaultVerifier>
+        {
+            TestCode = @"
+#nullable enable
+public class MyClass
+{
+    public int? {|#0:Count|} { get; set; }
+
+    public MyClass()
+    {
+        Count = 42;
+    }
+}
+",
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(DiagnosticDescriptors.NOTNULL001)
+                    .WithLocation(0)
+                    .WithArguments("Count"),
+            },
+        };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task NullableProperty_AssignedViaObjectInitializer_Reports()
+    {
+        var test = new CSharpAnalyzerTest<NullableAssignmentAnalyzer, DefaultVerifier>
+        {
+            TestCode = @"
+#nullable enable
+public class MyClass
+{
+    public string? {|#0:Name|} { get; set; }
+}
+
+public class Consumer
+{
+    public void Create()
+    {
+        var obj = new MyClass { Name = ""hello"" };
+    }
+}
+",
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(DiagnosticDescriptors.NOTNULL001)
+                    .WithLocation(0)
+                    .WithArguments("Name"),
+            },
+        };
+        await test.RunAsync();
+    }
 }
